4b7f9878f77583866dbb4c2c3865e376949b378c
Islamykh <137045688+Islamykh@users.noreply.github.com>
2023-09-02 07:43:15 +0300

    Edit search

--- JS/home.js
+++ JS/home.js
@@ -1,27 +1,78 @@
 
 function clicked(){
-  
-   
-swal.fire({
-  title: 'Loading Search',
-  html: 'Please Wait <b></b> .',
-  timer: 2000,
-  timerProgressBar: true,
-  didOpen: () => {
-    swal.showLoading()
-    const b = Swal.getHtmlContainer().querySelector('b')
-    timerInterval = setInterval(() => {
-      b.textContent = Swal.getTimerLeft()
-    }, 100)
-  },
-  willClose: () => {
-    clearInterval(timerInterval)
-  }
-}).then((result) => {
-  /* Read more about handling dismissals below */
-  if (result.dismiss === Swal.DismissReason.timer) {
-    console.log('I was closed by the timer')
-  }
-})
-
+  swal.fire({
+    title: 'Loading Search',
+    html: 'Please Wait <b></b> .',
+    timer: 2000,
+    timerProgressBar: true,
+    didOpen: () => {
+      swal.showLoading()
+      const b = Swal.getHtmlContainer().querySelector('b')
+      timerInterval = setInterval(() => {
+        b.textContent = Swal.getTimerLeft()
+      }, 100)
+    },
+    willClose: () => {
+      clearInterval(timerInterval)
+    }
+  }).then((result) => {
+    /* Read more about handling dismissals below */
+    if (result.dismiss === Swal.DismissReason.timer) {
+      console.log('I was closed by the timer')
+    }
+  });
 }
+
+
+// var IDchecked= document.getElementsByName("Id");
+// var Casechecked=document.getElementsByName("Case");
+// function chekNumber(){
+//  if((IDchecked !="") && (Casechecked != "")){
+//   if("-" in IDchecked ){
+//     if (("-" in Casechecked)){
+//       if("c" in Casechecked ){
+//         swal.fire({
+//           title: 'Loading Search',
+//           html: 'Please Wait <b></b> .',
+//           timer: 2000,
+//           timerProgressBar: true,
+//           didOpen: () => {
+//             swal.showLoading()
+//             const b = Swal.getHtmlContainer().querySelector('b')
+//             timerInterval = setInterval(() => {
+//               b.textContent = Swal.getTimerLeft()
+//             }, 100)
+//           },
+//           willClose: () => {
+//             clearInterval(timerInterval)
+//           }
+//         }).then((result) => {
+//           /* Read more about handling dismissals below */
+//           if (result.dismiss === Swal.DismissReason.timer) {
+//             console.log('I was closed by the timer')
+//           }
+//         });
+
+//       }
+//       else
+//       alert("Eroor! the case number must be have letter C");
+//     }
+//     else
+//     alert("Eroor! Please add "-" in your case number");}
+
+//   else
+//   alert("Eroor! Please add "-" in your case number");
+
+//  }
+//  else
+//  alert("Eroor! Please fill case number and ID number");
+
+
+// }
+
+
+
+
+
+
+

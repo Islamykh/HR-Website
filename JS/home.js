46b41bff8b3aed5110a8ae0faa6ec80b06c6c5f5
Islamykh <137045688+Islamykh@users.noreply.github.com>
2023-09-08 19:04:49 +0300

    Editing home page

--- JS/home.js
+++ JS/home.js
@@ -1,186 +1,51 @@
-var IDNum= document.getElementById("IDNum");
-var CaseNum= document.getElementById("CaseNum");
-IDNum.addEventListener("change",ChickNum)
-CaseNum.addEventListener("change",ChickNum)
-
 function ChickNum(){
-var truevalue= ["0","1","2","3","4","5","6","7","8","9","-","c"]
-var lengthIDNum=  document.getElementById("IDNum").value.length;
-console.log(lengthIDNum);
-console.log(truevalue.length)
+  var IDNum= document.getElementById("IDNum").value;
+  var CaseNum= document.getElementById("CaseNum");
+  var truevalue= ["0","1","2","3","4","5","6","7","8","9","-","c"];
+  var l= IDNum.length;
+  
+  
+  if(l==12)
+    {
+      for(var i=0;i<l;i++)
+        {
+          if(truevalue.includes(IDNum[i], 0)) {
+            let timerInterval
+            Swal.fire({
+              title: 'Auto close alert!',
+              html: 'I will close in <b></b> milliseconds.',
+              timer: 2000,
+              timerProgressBar: true,
+              didOpen: () => {
+                Swal.showLoading()
+                const b = Swal.getHtmlContainer().querySelector('b')
+                timerInterval = setInterval(() => {
+                  b.textContent = Swal.getTimerLeft()
+                }, 100)
+              },
+              willClose: () => {
+                clearInterval(timerInterval)
+              }
+            }).then((result) => {
+              /* Read more about handling dismissals below */
+              if (result.dismiss === Swal.DismissReason.timer) {
+                console.log('I was closed by the timer')
+              }
+              })
+              continue;
 
-if(lengthIDNum ==12){ // because the number of numbers at ID Number is equal 12 
-  for(var i=0 ; i < lengthIDNum ; i++ ){ // to check the numbers: by number number :)
-    for(var j=0; j < truevalue.length-1 ; j++){
-      if(IDNum.value[i]==truevalue[j]){
-
-        function clicked(){
-
-          swal.fire({
-          title: 'Loading Search',
-          timer: 2000,
-          timerProgressBar: true,
-          didOpen: () => {
-            swal.showLoading()
-            const b = Swal.getHtmlContainer().querySelector('b')
-            timerInterval = setInterval(() => {
-              b.textContent = Swal.getTimerLeft()
-            }, 100)
-          },
-          willClose: () => {
-            clearInterval(timerInterval)
           }
-        }).then((result) => {
-          /* Read more about handling dismissals below */
-          if (result.dismiss === Swal.DismissReason.timer) {
-            console.log('I was closed by the timer')
+          
+          else 
+          {
+            document.getElementById("ErrorMass").style.display="block";
+            break;
           }
-        });
-        }
-      }
-      
-      else{
-        document.getElementById("ErrorMass").style.display="block";
-
         
-      }
-}
-}
-}
-}
+        }
+    }
+    else{
+      document.getElementById("ErrorMass3").style.display="block";
 
-
-
-
-// /* Function */
-
-// function 
-// doThing(){
-
-// console.log(typeof(search.value))
-
-
-// var check = 
-// false;
-
-// for (var i = 0; i < search.value.length; i++){
-//for (varj=0;j<trueValues.length;j++){
-
-// if (search.value[i]== trueValues[j]){
-
-// console.log(typeof(search.value[i]))
-
-// console.log(typeof(trueValues[j]))
-
-// check 
-// = true;
-
-// console.log("true")
-
-// break;
-
-// }
-
-// else{
-// check 
-// = false;
-// console.log("false")
-// }
-// }
-// if(check
-// == 
-// false){
-// alert("Eroor! Please add only number in your ID number");
-
-// document.getElementById("errorP").style.display
-// = 
-// "block";
-// }
-// else{
-// }
-// }
-// }
-
-
-
-
-
-
-
-// function clicked(){
-//   swal.fire({
-//     title: 'Loading Search',
-//     html: 'Please Wait <b></b> .',
-//     timer: 2000,
-//     timerProgressBar: true,
-//     didOpen: () => {
-//       swal.showLoading()
-//       const b = Swal.getHtmlContainer().querySelector('b')
-//       timerInterval = setInterval(() => {
-//         b.textContent = Swal.getTimerLeft()
-//       }, 100)
-//     },
-//     willClose: () => {
-//       clearInterval(timerInterval)
-//     }
-//   }).then((result) => {
-//     /* Read more about handling dismissals below */
-//     if (result.dismiss === Swal.DismissReason.timer) {
-//       console.log('I was closed by the timer')
-//     }
-//   });
-// }
-
-
-// var IDchecked= document.getElementsByName("Id");
-// var Casechecked=document.getElementsByName("Case");
-// function chekNumber(){
-//  if((IDchecked !="") && (Casechecked != "")){
-//   if("-" in IDchecked ){
-//     if (("-" in Casechecked)){
-//       if("c" in Casechecked ){
-//         swal.fire({
-//           title: 'Loading Search',
-//           html: 'Please Wait <b></b> .',
-//           timer: 2000,
-//           timerProgressBar: true,
-//           didOpen: () => {
-//             swal.showLoading()
-//             const b = Swal.getHtmlContainer().querySelector('b')
-//             timerInterval = setInterval(() => {
-//               b.textContent = Swal.getTimerLeft()
-//             }, 100)
-//           },
-//           willClose: () => {
-//             clearInterval(timerInterval)
-//           }
-//         }).then((result) => {
-//           /* Read more about handling dismissals below */
-//           if (result.dismiss === Swal.DismissReason.timer) {
-//             console.log('I was closed by the timer')
-//           }
-//         });
-
-//       }
-//       else
-//       alert("Eroor! the case number must be have letter C");
-//     }
-//     else
-//     alert("Eroor! Please add "-" in your case number");}
-
-//   else
-//   alert("Eroor! Please add "-" in your case number");
-
-//  }
-//  else
-//  alert("Eroor! Please fill case number and ID number");
-
-
-// }
-
-
-
-
-
-
-
+    }
+  }
--- JS/signup.js
+++ JS/signup.js
@@ -14,3 +14,33 @@ function messaGe(){
         }
       })
 }
+// var Name = document.getElementById("name").value.length;
+// Name= addEventListener("change", checkfunc);
+// var IDNumber=  document.getElementById("IDNumber").value.length;
+// IDNumber= addEventListener("change", checkfunc);
+// var caseNum= document.getElementById("caseNum").value;
+// caseNum= addEventListener("change", checkfunc);
+// var pass= document.getElementById("pass").value;
+// pass= addEventListener("change", checkfunc);
+// var BDay= document.getElementById("BDay").value;
+// BDay= addEventListener("change", checkfunc);
+// var PHNum =document.getElementById("PHNum").value;
+// PHNum= addEventListener("change", checkfunc);
+// function checkfunc(){
+//   if(Name == 0){
+//     document.getElementById("errorname").style.display="block";
+//     console.log(10);
+//   }
+//   else{
+//     console.log("1000");
+//   }
+//   if (IDNumber!="11"){
+//     document.getElementById("errorID").style.display="block";
+//     console.log(20);
+//   }
+//   else{
+//     console.log(50);
+
+//   }
+// }
+
